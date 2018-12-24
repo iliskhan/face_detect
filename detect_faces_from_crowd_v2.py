8f86d1f5e40c6ea18f6623de028a2e4bd7eaedfb
iliskhan_gudaev <iliskhan234@gmail.com>
2018-12-24 18:12:24 +0300

    shift2

--- detect_faces_from_crowd_v2.py
+++ detect_faces_from_crowd_v2.py
@@ -1,33 +1,73 @@
-import dlib
-import cv2
-import numpy as np
 import os
+import dlib
+import cv2 as cv
+import numpy as np
 
+from time import time 
 from queue import Queue
 from scipy.spatial import distance
 from multiprocessing import Pool, Manager, Process, Queue
 
-def worker(images_q):
-	cap = cv2.VideoCapture(0)
-	for i in range(10):
-		img = cap.read()[1]
-		images_q.put(img)
-	cap.release()
 
+def histogramic(track_window, frame):
+
+	r,h,c,w = track_window
+	roi = frame[r:r+h, c:c+w]
+	hsv_roi =  cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+	mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+	roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
+	cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
+
+	term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
+	return roi_hist, term_crit
+
+def worker(images_q):
+	
+	cap = cv.VideoCapture(0)
+	while cap.isOpened():
+		images_q.put(cap.read()[1])
+	cap.release()
+	
 def main():
 
 	detector = dlib.get_frontal_face_detector()
 	sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
 	facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
+	
 	manager = Manager()
 	images_q = manager.Queue(25)
 
-	p = Process(target=worker, args=(images_q,), daemon=True)
-	p.start()
-	p.join()
-	for i in range(10):
-		images_q.get()
-		print(i)
+	Process(target=worker, args=(images_q,), daemon=True).start()
 
+	
+	while True:
+		while True:
+			if not images_q.empty():
+				img = images_q.get()
+				break
+
+		dets = detector(img, 1)
+		track_windows = tuple((d.top(), d.height(), d.left(), d.width()) for d in dets)
+		histograms = tuple(map(histogramic, track_windows, (img for i in track_windows)))
+		for step in range(24):
+			frame = images_q.get()
+			hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+			i = 0
+			for roi_hist, term_crit in histograms:
+				dst = cv.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+				# apply meanshift to get the new location
+				ret, track_window = cv.CamShift(dst, track_windows[i], term_crit)
+				# Draw it on image
+				pts = cv.boxPoints(ret)
+				pts = np.int0(pts)
+				img2 = cv.polylines(frame,[pts],True, 255,2)
+				cv.imshow('img2',img2)
+				k = cv.waitKey(60) & 0xff
+				if k == 27:
+					break
+				else:
+					cv.imwrite(chr(k)+".jpg",img2)
+				i+=1
 if __name__ == '__main__':
 	main()

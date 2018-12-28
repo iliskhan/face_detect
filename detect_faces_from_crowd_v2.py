6c14c1718968e8d11a685a9629358c32570cf5f6
iliskhan_gudaev <iliskhan234@gmail.com>
2018-12-28 16:36:39 +0300

    add tracking with dlib

--- detect_faces_from_crowd_v2.py
+++ detect_faces_from_crowd_v2.py
@@ -9,19 +9,16 @@ from scipy.spatial import distance
 from multiprocessing import Pool, Manager, Process, Queue
 
 def main():
-
-	detector = dlib.get_frontal_face_detector()
-	sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
-	facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
+	FRAME_COUNT = 25
 	
 	manager = Manager()
-	images_q = manager.Queue(25)
+	images_q = manager.Queue(FRAME_COUNT)
+	points_array = manager.Queue(1)
 
 	Process(target=worker, args=(images_q,), daemon=True).start()
 
-	Process(target)
+	Process(target=det_and_rec, args=(images_q, points_array), daemon=True).start()
 
-	flag = True
 	close = False
 	
 	while not close:
@@ -30,31 +27,40 @@ def main():
 				img = images_q.get()
 				break
 
-		dets = detector(img, 1)
-		if len(dets):
-			track_windows = tuple((d.top(), d.height(), d.left(), d.width()) for d in dets)
-			histograms = tuple(map(histogramic, track_windows, [img for i in track_windows]))
-			for step in range(24):
+		if points_array.full():
+			dets = points_array.get()
+
+			if len(dets):
+				track_windows = tuple((d.top(), d.height(), d.left(), d.width()) for d in dets)
+				histograms = tuple(map(histogramic, track_windows, [img for i in track_windows]))
+				for step in range(FRAME_COUNT-1):
+					frame = images_q.get()
+					hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+					i = 0
+					for roi_hist, term_crit in histograms:
+						dst = cv.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+
+						ret, track_window = cv.CamShift(dst, track_windows[i], term_crit)
+						
+						pts = cv.boxPoints(ret)
+						pts = np.int0(pts)
+						frame = cv.polylines(frame,[pts],True, 255,2)
+						i+=1
+
+					cv.imshow('img2',frame)
+					k = cv.waitKey(60) & 0xff
+					if k == 27:
+						close = True
+						break
+
+			else:
 				frame = images_q.get()
-				hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
-				i = 0
-				for roi_hist, term_crit in histograms:
-					dst = cv.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-
-					ret, track_window = cv.CamShift(dst, track_windows[i], term_crit)
-					
-					pts = cv.boxPoints(ret)
-					pts = np.int0(pts)
-					frame = cv.polylines(frame,[pts],True, 255,2)
-					i+=1
-
 				cv.imshow('img2',frame)
 				k = cv.waitKey(60) & 0xff
 				if k == 27:
 					close = True
 					break
-
 		else:
 			frame = images_q.get()
 			cv.imshow('img2',frame)
@@ -63,6 +69,48 @@ def main():
 				close = True
 				break
 			
+def det_and_rec(images_q, points_array):
+	num_map = np.vectorize(distance.euclidean, signature='(n),(m)->()')
+
+	detector = dlib.get_frontal_face_detector()
+	sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
+	facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
+
+	matrix_descriptors = None
+	while True:
+		while True:
+			if not images_q.empty():
+				frame = images_q.get()
+				break
+
+		dets = detector(frame, 1)
+		if points_array.full():
+			points_array.get()
+			points_array.put(dets)
+		else:
+			points_array.put(dets)
+
+		for d in dets:
+			shape = sp(frame, d)
+			face_descriptor = facerec.compute_face_descriptor(frame, shape)
+			face_descriptor = np.array([face_descriptor])
+				
+			if matrix_descriptors is not None:
+				print(f'matrix_descriptors: {matrix_descriptors.shape}')
+
+				vector_of_differences = num_map(face_descriptor,matrix_descriptors)
+				index = np.argmin(vector_of_differences)
+
+				if vector_of_differences[index] <= 0.7:
+					cv.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (255,0,255), 2)
+
+				else:
+					
+					cv.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0,0,255), 2)
+					matrix_descriptors = np.append(matrix_descriptors, face_descriptor, axis=0)
+
+			else:
+				matrix_descriptors = face_descriptor
 
 def histogramic(track_window, frame):
 
--- detect_faces_from_crowd_v3.py
+++ detect_faces_from_crowd_v3.py
@@ -8,108 +8,49 @@ import dlib
 import imutils
 import numpy as np
 
+from time import time
 from scipy.spatial import distance
-from multiprocessing import Pool, Manager
+from multiprocessing.dummy import Pool
 from PIL import ImageTk, Image, ImageDraw, ImageFont
-
-detector = dlib.get_frontal_face_detector()
-sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
-facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
-
-def img_generator(cap, w, h):
-	while cap.isOpened():
-
-		yield cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB), w, h
-
-def video_maker(frames_with_data):
-
-
-	frame, w, h = frames_with_data
-
-	dets = detector(frame, 1)
-	
-	descriptors = []
-
-	for d in dets:
-		shape = sp(frame, d)
-		face_descriptor = facerec.compute_face_descriptor(frame, shape)
-		
-		descriptors.append(face_descriptor)
-			
-		cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0,0,255), 2)
-
-	return frame, descriptors
+from imutils.video import VideoStream, FPS
 
 def main():
+	detector = dlib.get_frontal_face_detector()
+	# sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
+	# facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
 
 	num_map = np.vectorize(distance.euclidean, signature='(n),(m)->()')
 
-	#matrix_descriptors = None
+	cap = cv2.VideoCapture(0)   #'data/' + dirs[0]
+	fps = FPS().start()
+	counter = 0
+	while True:
+		frame = cap.read()[1]
+		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		fps.update()
+		if counter % 50 == 0:
+			trackers = []
+			dets = detector(rgb, 1)
+			for d in dets:
+				tracker = dlib.correlation_tracker()
+				tracker.start_track(rgb, d)
+				trackers.append(tracker)
 
-	dirs = os.listdir('data')
-
-	cap = cv2.VideoCapture(0)    #'data/' + dirs[0]
-	# cap.set(3,1080)
-	# cap.set(4,720)
-	#out = cv2.VideoWriter('res/output6.mp4', CODEC, 25.0, (w,h))
-	img = imutils.resize(cap.read()[1], height=720)
-	h, w = img.shape[:2]
-	w = int(w * 0.7)
-	h = int(h * 0.9)
-
-	fnt = ImageFont.truetype("arial.ttf", 35)
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	matrix_descriptors = np.array([])
-	window = Tk()
-	window.title('Счетчик')
-
-	with Pool() as p:
-		for d, i in enumerate(p.imap(video_maker, img_generator(cap,w,h), chunksize=1)):
-			for child in window.winfo_children():
-				child.destroy()
-
-			frame = i[0]
-
-			for y in i[1]:
-				face_descriptor = np.array([y])
-				
-				if len(matrix_descriptors) > 0:
-					
-					vector_of_differences = num_map(face_descriptor,matrix_descriptors)
-					index = np.argmin(vector_of_differences)
-
-					if vector_of_differences[index] >= 0.6:
-						matrix_descriptors = np.append(matrix_descriptors, face_descriptor, axis=0)
-
-				elif len(matrix_descriptors) == 0:
-
-					matrix_descriptors = face_descriptor
-
-			frame = Image.fromarray(imutils.resize(frame, height=720))
-			draw = ImageDraw.Draw(frame)
-
-			draw.text((w, h), f'Уникальных: {len(matrix_descriptors)}', font=fnt, fill=(255,0,0))
-
-			photo = ImageTk.PhotoImage(frame)
-
-			label_with_image = Label(window, image=photo)
-			label_with_image.image = photo
-
-			ok_button = Button(window,
-						text="Остановить",
-						command=lambda: cap.release())
-
-			label_with_image.pack()
-			ok_button.pack(padx=40,pady=10,side=LEFT)
-			window.update()
-
-			print(f'кадр {d} записан')
-		p.close()
-		p.join()
-
+		elif len(trackers) > 0:
+			for tracker in trackers:
+				confidence = tracker.update(rgb)
+				if confidence > 10:
+					drect = tracker.get_position()
+					left, top, right, bottom = tuple(map(int, (drect.left(), drect.top(), drect.right(), drect.bottom())))
+					cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)
+		counter+=1
+		cv2.imshow('img', frame)
+		k = cv2.waitKey(1) & 0xff
+		if k == ord('q'):
+			break
 	cap.release()
-	#out.release()
-	window.mainloop()
+	fps.stop()
+	print(f"{fps.fps():.2f}")
 
 if __name__ == '__main__':
 	main()

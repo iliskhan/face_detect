dd1ad87ebfdf91daa8c865a376e7e72ab6b3d4c0
iliskhan_gudaev <iliskhan234@gmail.com>
2018-12-29 09:16:52 +0300

    upgrade processing

--- detect_faces_from_crowd_v3.py
+++ detect_faces_from_crowd_v3.py
@@ -1,6 +1,3 @@
-from tkinter import *
-from tkinter.ttk import *
-
 import os
 import sys
 import cv2
@@ -10,47 +7,81 @@ import numpy as np
 
 from time import time
 from scipy.spatial import distance
-from multiprocessing.dummy import Pool
+from multiprocessing import Process, Manager
 from PIL import ImageTk, Image, ImageDraw, ImageFont
 from imutils.video import VideoStream, FPS
 
 def main():
-	detector = dlib.get_frontal_face_detector()
-	# sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
-	# facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
+	
+	manager = Manager()
+	images_q = manager.Queue(25)
+	dets_q = manager.Queue(1)
+
+	Process(target=capturing_process, args=(images_q,), daemon=True).start()
+	Process(target=detecting_process, args=(images_q, dets_q), daemon=True).start()
 
 	num_map = np.vectorize(distance.euclidean, signature='(n),(m)->()')
 
-	cap = cv2.VideoCapture(0)   #'data/' + dirs[0]
 	fps = FPS().start()
+	trackers = []
 	counter = 0
 	while True:
-		frame = cap.read()[1]
-		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		fps.update()
-		if counter % 50 == 0:
-			trackers = []
-			dets = detector(rgb, 1)
-			for d in dets:
-				tracker = dlib.correlation_tracker()
-				tracker.start_track(rgb, d)
-				trackers.append(tracker)
+		if not images_q.empty() and not images_q.qsize() < 10:
+			frame = images_q.get()
+			rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+			fps.update()
 
-		elif len(trackers) > 0:
-			for tracker in trackers:
-				confidence = tracker.update(rgb)
-				if confidence > 10:
-					drect = tracker.get_position()
-					left, top, right, bottom = tuple(map(int, (drect.left(), drect.top(), drect.right(), drect.bottom())))
-					cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)
-		counter+=1
-		cv2.imshow('img', frame)
-		k = cv2.waitKey(1) & 0xff
-		if k == ord('q'):
-			break
-	cap.release()
+			if counter % 25 == 0:
+				counter = 0
+				if not dets_q.empty():
+					trackers = []
+					dets, rgb = dets_q.get()
+					for d in dets:
+						tracker = dlib.correlation_tracker()
+
+						tracker.start_track(rgb, d)
+						trackers.append(tracker)
+
+			elif len(trackers) > 0:
+				for tracker in trackers:
+					confidence = tracker.update(rgb)
+					if confidence > 8:
+						drect = tracker.get_position()
+						left, top, right, bottom = tuple(map(int, (drect.left(), drect.top(), drect.right(), drect.bottom())))
+						cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)
+			counter+=1
+			cv2.imshow('img', frame)
+			k = cv2.waitKey(10) & 0xff
+			if k == ord('q'):
+				break
 	fps.stop()
-	print(f"{fps.fps():.2f}")
+	print(f"fps = {fps.fps():.2f}")
+
+def capturing_process(images_q):
+	cap = cv2.VideoCapture(0)
+	while cap.isOpened():
+		if images_q.full():
+			images_q.get()
+			images_q.put(cap.read()[1])
+		else:
+			images_q.put(cap.read()[1])
+
+	cap.release()
+
+def detecting_process(images_q, dets_q):
+	detector = dlib.get_frontal_face_detector()
+	sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
+	facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
+
+	while not images_q.empty():
+		rgb = cv2.cvtColor(images_q.get(), cv2.COLOR_BGR2RGB)
+
+		dets = detector(rgb, 1)
+		if dets_q.full():
+			dets_q.get()
+			dets_q.put((dets, rgb))
+		else:
+			dets_q.put((dets, rgb))	
 
 if __name__ == '__main__':
 	main()

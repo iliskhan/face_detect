64c555634c2552c96fb537ead44a1383b675336d
iliskhan <iliskhan234@gmail.com>
2019-01-03 11:56:56 +0300

    added extra process

--- detect_faces_from_crowd_v3.py
+++ detect_faces_from_crowd_v3.py
@@ -12,26 +12,28 @@ from PIL import ImageTk, Image, ImageDraw, ImageFont
 from imutils.video import VideoStream, FPS
 
 def main():
-	print('blas',dlib.DLIB_USE_BLAS)
-	print('cuda',dlib.DLIB_USE_CUDA)
-	print('lapack',dlib.DLIB_USE_LAPACK)
-	print('avx',dlib.USE_AVX_INSTRUCTIONS)
-	print('neon',dlib.USE_NEON_INSTRUCTIONS)
+	print('blas -', dlib.DLIB_USE_BLAS)
+	print('cuda -', dlib.DLIB_USE_CUDA)
+	print('lapack -', dlib.DLIB_USE_LAPACK)
+	print('avx -', dlib.USE_AVX_INSTRUCTIONS)
+	print('neon -', dlib.USE_NEON_INSTRUCTIONS)
 	
 	manager = Manager()
-	images_q = manager.Queue(25)
 	dets_q = manager.Queue(1)
+	images_q = manager.Queue(25)
+	q_for_detproc = manager.Queue(1)
 
-	Process(target=capturing_process, args=(images_q,), daemon=True).start()
-	Process(target=detecting_process, args=(images_q, dets_q), daemon=True).start()
+	Process(target=detecting_process, args=(q_for_detproc, dets_q), daemon=True).start()
+	Process(target=capturing_process, args=(images_q, q_for_detproc), daemon=True).start()
 
 	num_map = np.vectorize(distance.euclidean, signature='(n),(m)->()')
 
-	fps = FPS().start()
-	trackers = []
 	counter = 0
+	trackers = []
+	fps = FPS().start()
+
 	while True:
-		if not images_q.empty() and not images_q.qsize() < 10:
+		if images_q.qsize() > 20:
 			frame = images_q.get()
 			rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 			fps.update()
@@ -43,7 +45,6 @@ def main():
 					dets, rgb = dets_q.get()
 					for d in dets:
 						tracker = dlib.correlation_tracker()
-
 						tracker.start_track(rgb, d)
 						trackers.append(tracker)
 
@@ -62,35 +63,38 @@ def main():
 	fps.stop()
 	print(f"fps = {fps.fps():.2f}")
 
-def capturing_process(images_q):
+def capturing_process(images_q, q_for_detproc):
 	cap = cv2.VideoCapture('rep.mp4')
 	while cap.isOpened():
-		img = imutils.resize(cap.read()[1], width=700)
+		img = imutils.resize(cap.read()[1], width=1000)
 		# if images_q.full():
 		# 	images_q.get()
 		# 	images_q.put(img)
 		# else:
 		images_q.put(img)
+		if q_for_detproc.empty():
+			q_for_detproc.put(img)
 
 	cap.release()
 
-def detecting_process(images_q, dets_q):
+def detecting_process(q_for_detproc, dets_q):
 	detector = dlib.get_frontal_face_detector()
 	sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
 	facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
 	while True:
-		while not images_q.empty():
-			rgb = cv2.cvtColor(images_q.get(), cv2.COLOR_BGR2RGB)
+		while not q_for_detproc.empty():
+			rgb = cv2.cvtColor(q_for_detproc.get(), cv2.COLOR_BGR2RGB)
 			lt = time()
-			dets = detector.run(rgb,2,1)
+			dets = detector.run(rgb,1)
 			
 			for d, conf, orient in zip(*dets):
 
 				if conf < 1:
-					print(conf)
+
 					dets[0].remove(d)
 					dets[1].remove(conf)
 					dets[2].remove(orient)
+
 			print(time()-lt)
 			if dets_q.full():
 				dets_q.get()

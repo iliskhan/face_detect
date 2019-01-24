ec8d032784e0eaeb66e48c0f353cbc2a74fc6901
iliskhan_gudaev <iliskhan234@gmail.com>
2019-01-24 14:23:19 +0300

    v2 up

--- detect_faces_from_crowd_v2.py
+++ detect_faces_from_crowd_v2.py
@@ -1,140 +1,118 @@
 import os
+import cv2
 import dlib
-import cv2 as cv
+import imutils
 import numpy as np
 
-from time import time 
-from queue import Queue
+from time import time
+from multiprocessing import Pool
 from scipy.spatial import distance
-from multiprocessing import Pool, Manager, Process, Queue
+
+detector = dlib.get_frontal_face_detector()
+
+sp = dlib.shape_predictor(
+	'models/shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1(
+	'models/dlib_face_recognition_resnet_model_v1.dat')
+
+def img_croper(cap):
+
+	frame = cap.read()[1]
+
+	height, width = frame.shape[:2]
+	h_half, w_half = height//2, width//2 
+	h_coords, w_coords = [0, h_half, height], [0, w_half, width]
+	while True:
+		frame = cap.read()[1]
+		image_chunks = tuple(frame[h_coords[i]:h_coords[i+1],
+								     w_coords[j]:w_coords[j+1]] 
+								     	for i in range(len(h_coords)-1) 
+								     	for j in range(len(w_coords)-1))
+
+		# left_top = frame[h_coords[0]:h_coords[1], w_coords[0]:w_coords[1]]
+		# right_top = frame[h_coords[0]:h_coords[1], w_coords[1]:w_coords[2]]
+		# left_bottom = frame[h_coords[1]:h_coords[2], w_coords[0]:w_coords[1]]
+		# right_bottom = frame[h_coords[1]:h_coords[2], w_coords[1]:w_coords[2]]
+
+		yield image_chunks, frame
+
+def process_face_detect(frame):
+
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	dets = detector.run(rgb,0,1)
+	face_descriptors = []
+	if len(dets[0]) != 0:
+		
+		for d, conf, orient in zip(*dets):
+			if orient != 0:
+				dets[0].remove(d)
+				dets[1].remove(conf)
+				dets[2].remove(orient)
+				continue
+			
+			shape = sp(rgb, d)
+
+			face_descriptor = facerec.compute_face_descriptor(rgb, shape)
+			face_descriptors.append(face_descriptor)
+	return face_descriptors, dets[0]
 
 def main():
-	FRAME_COUNT = 25
-	
-	manager = Manager()
-	images_q = manager.Queue(FRAME_COUNT)
-	points_array = manager.Queue(1)
 
-	Process(target=worker, args=(images_q,), daemon=True).start()
+	print('blas -'  , dlib.DLIB_USE_BLAS)
+	print('cuda -'  , dlib.DLIB_USE_CUDA)
+	print('lapack -', dlib.DLIB_USE_LAPACK)
+	print('avx -'   , dlib.USE_AVX_INSTRUCTIONS)
+	print('neon -'  , dlib.USE_NEON_INSTRUCTIONS)
 
-	Process(target=det_and_rec, args=(images_q, points_array), daemon=True).start()
+	font = cv2.FONT_HERSHEY_SIMPLEX
 
-	close = False
-	
-	while not close:
-		while True:
-			if not images_q.empty():
-				img = images_q.get()
-				break
-
-		if points_array.full():
-			dets = points_array.get()
-
-			if len(dets):
-				track_windows = tuple((d.top(), d.height(), d.left(), d.width()) for d in dets)
-				histograms = tuple(map(histogramic, track_windows, [img for i in track_windows]))
-				for step in range(FRAME_COUNT-1):
-					frame = images_q.get()
-					hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
-					i = 0
-					for roi_hist, term_crit in histograms:
-						dst = cv.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-
-						ret, track_window = cv.CamShift(dst, track_windows[i], term_crit)
-						
-						pts = cv.boxPoints(ret)
-						pts = np.int0(pts)
-						frame = cv.polylines(frame,[pts],True, 255,2)
-						i+=1
-
-					cv.imshow('img2',frame)
-					k = cv.waitKey(60) & 0xff
-					if k == 27:
-						close = True
-						break
-
-			else:
-				frame = images_q.get()
-				cv.imshow('img2',frame)
-				k = cv.waitKey(60) & 0xff
-				if k == 27:
-					close = True
-					break
-		else:
-			frame = images_q.get()
-			cv.imshow('img2',frame)
-			k = cv.waitKey(60) & 0xff
-			if k == 27:
-				close = True
-				break
-			
-def det_and_rec(images_q, points_array):
 	num_map = np.vectorize(distance.euclidean, signature='(n),(m)->()')
+	cap = cv2.VideoCapture(1)
+	cap.set(3,1500)
 
-	detector = dlib.get_frontal_face_detector()
-	sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
-	facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
+	frame = cap.read()[1]
+	height, width = frame.shape[:2]
 
-	matrix_descriptors = None
-	while True:
-		while True:
-			if not images_q.empty():
-				frame = images_q.get()
+	matrix_of_descriptors = 0
+	count = 0
+	with Pool() as p:
+		
+		for image_chunks, frame in img_croper(cap):
+			
+			data = p.map(process_face_detect, image_chunks)
+			for face_descriptors, dets in data:
+				for i in range(len(face_descriptors)):
+					face_descriptor = face_descriptors[i]
+					d = dets[i]
+
+					face_descriptor = np.array([face_descriptor])
+					rgb_copy = frame[d.top():d.bottom(), d.left():d.right()]
+
+					if type(matrix_of_descriptors) is int:
+						matrix_of_descriptors = face_descriptor
+						cv2.imwrite(f'{len(matrix_of_descriptors)}.jpg', rgb_copy)
+						count+=1
+
+					else:
+						vector_of_differences = num_map(face_descriptor,matrix_of_descriptors)
+
+						index = np.argmin(vector_of_differences)
+						if vector_of_differences[index] >= 0.6:
+
+							matrix_of_descriptors = np.append(matrix_of_descriptors, face_descriptor, axis=0)
+							count+=1
+							
+							cv2.imwrite(f'{len(matrix_of_descriptors)}.jpg', rgb_copy)
+
+			cv2.putText(frame, str(count), (width-100, height-100) , font, 4,(255,255,255),2,cv2.LINE_AA)
+			cv2.imshow('frame',frame)
+			k = cv2.waitKey(3) & 0xff
+			if k == 27:
 				break
-
-		dets = detector(frame, 1)
-		if points_array.full():
-			points_array.get()
-			points_array.put(dets)
-		else:
-			points_array.put(dets)
-
-		for d in dets:
-			shape = sp(frame, d)
-			face_descriptor = facerec.compute_face_descriptor(frame, shape)
-			face_descriptor = np.array([face_descriptor])
-				
-			if matrix_descriptors is not None:
-				print(f'matrix_descriptors: {matrix_descriptors.shape}')
-
-				vector_of_differences = num_map(face_descriptor,matrix_descriptors)
-				index = np.argmin(vector_of_differences)
-
-				if vector_of_differences[index] <= 0.7:
-					cv.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (255,0,255), 2)
-
-				else:
-					
-					cv.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0,0,255), 2)
-					matrix_descriptors = np.append(matrix_descriptors, face_descriptor, axis=0)
-
-			else:
-				matrix_descriptors = face_descriptor
-
-def histogramic(track_window, frame):
-
-	r,h,c,w = track_window
-	roi = frame[r:r+h, c:c+w]
-	hsv_roi =  cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-	mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-	roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
-	cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
-
-	term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
-	return roi_hist, term_crit
-
-def worker(images_q):
-	
-	cap = cv.VideoCapture(0)
-	while cap.isOpened():
-		if images_q.full():
-			images_q.get()
-			images_q.put(cap.read()[1])
-		else:
-			images_q.put(cap.read()[1])
 
 	cap.release()
-	
+	cv2.destroyAllWindows()
+
 if __name__ == '__main__':
 	main()
